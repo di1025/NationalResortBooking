c11ffd5cfe1ad60aaa7423d49366ba3b7ae17c87
Di Chen <chendi1025@gmail.com>
2018-12-28 00:23:59 -0500

    pass UserRepositoryTest

--- src/test/java/com/chendi/project/repository/PaymentRepositoryTest.java
+++ src/test/java/com/chendi/project/repository/PaymentRepositoryTest.java
@@ -22,17 +22,56 @@ import static org.junit.Assert.assertEquals;
 @ActiveProfiles("unit")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class PaymentRepositoryTest {
-////    @Query("select p from payments where p.order_id = ?1")
-////    List<Payment> findByOrderIdis(Long id);
-////
-////    List<Payment> findByLastnameAndFirstname(String holderFristName, String holderLastName);
+//    @Query("select p from payments where p.order_id = ?1")
+//    List<Payment> findByOrderIdis(Long id);
 //
-//    @Autowired
-//    private PaymentRepository paymentRepository;
-//
+//    List<Payment> findByLastnameAndFirstname(String holderFristName, String holderLastName);
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
 //    @Test
 //    @Transactional
-//    public void findByOrderIdis() {
+//    public void findByOrderIdTest() {
 //        Payment p = new Payment();
+//        p.setBillingAddress1("testBillingAddress1");
+//        p.setBillingAddress2("testBillingAddress2");
+//        p.setBillingCity("testCity");
+//        p.setBillingCountry("testCountry");
+//        p.setBillingPhoneNum("88888888");
+//        p.setBillingState("testST");
+//        p.setBillingZipCode(1000000);
+//        p.setCardNumber(123);
+//        p.setCardType("Visa");
+//        p.setHolderFristName("testFN");
+//        p.setHolderLastName("testLN");
+//        paymentRepository.save(p);
+//        List<Payment> testPayments = paymentRepository.findByOrderId(p.getOrderId());
+//        assertNotNull(testPayments);
+//        assertEquals(p.getId(),testPayments.get(0).getId())
+//
+//---order is null so cant pass. orderid is fk, how to retrieve?
+//
 //    }
+    @Test
+    @Transactional
+    public void findByFirstNameAndLastNameTest(){
+        Payment p = new Payment();
+        p.setBillingAddress1("testBillingAddress1");
+        p.setBillingAddress2("testBillingAddress2");
+        p.setBillingCity("testCity");
+        p.setBillingCountry("testCountry");
+        p.setBillingPhoneNum("88888888");
+        p.setBillingState("testST");
+        p.setBillingZipCode(1000000);
+        p.setCardNumber(123);
+        p.setCardType("Visa");
+        p.setHolderFirstName("testFN");
+        p.setHolderLastName("testLN");
+        paymentRepository.save(p);
+        List<Payment> testPayments = paymentRepository.findByFirstNameAndLastName(p.getHolderFirstName(),p.getHolderLastName());
+        assertNotNull(testPayments);
+        assertEquals(p.getId(),testPayments.get(0).getId());
+        //---order is null so cant pass. orderid is fk, how to retrieve?
+    }
 }
